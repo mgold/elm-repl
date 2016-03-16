1acb6916dee82c2d0694f6173faec69be62feaf0
Max Goldstein <maxgoldstein1@gmail.com>
2016-03-16 16:40:18 -0400

    Add ans functionality

--- src/Environment.hs
+++ src/Environment.hs
@@ -4,6 +4,7 @@ module Environment where
 import Control.Monad.RWS (RWST, runRWST)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS
+import Data.List (isSubsequenceOf, words, unwords)
 import Data.Monoid ((<>))
 import Data.Trie (Trie) -- TODO: Switch to a Char-based trie.
 import qualified Data.Trie as Trie
@@ -59,6 +60,7 @@ data Env = Env
     , imports :: Trie String
     , adts :: Trie String
     , defs :: Trie String
+    , ansN :: Integer
     }
     deriving Show
 
@@ -71,6 +73,7 @@ empty compiler interpreter =
         Trie.empty
         Trie.empty
         (Trie.singleton firstVar (BS.unpack firstVar <> " = ()"))
+        0
 
 
 firstVar :: ByteString
@@ -80,7 +83,7 @@ firstVar =
 
 lastVar :: ByteString
 lastVar =
-    "deltron3030"
+    "ans"
 
 
 lastVarString :: String
@@ -97,36 +100,68 @@ toElmCode env =
 
 
 insert :: (Maybe DefName, String) -> Env -> Env
-insert (maybeName, src) env =
+insert (maybeName, body) env =
     case maybeName of
       Nothing ->
-          display src env
+          display body env
 
       Just (Import name) ->
           noDisplay $ env
-              { imports = Trie.insert (BS.pack name) src (imports env)
+              { imports = Trie.insert (BS.pack name) body (imports env)
               }
 
       Just (DataDef name) ->
           noDisplay $ env
-              { adts = Trie.insert (BS.pack name) src (adts env)
+              { adts = Trie.insert (BS.pack name) body (adts env)
               }
 
       Just (VarDef name) ->
-          define (BS.pack name) src (display name env)
+          define (BS.pack name) (replaceAns env body) (display name env)
+
+
+defineAns :: String -> Env -> Env
+defineAns body env =
+    env { defs = Trie.insert lastVar (formatDefinition lastVarString oldAnsString) $
+                 Trie.insert oldAns  (formatDefinition oldAnsString body) $
+                 defs env
+        , ansN = succ (ansN env)
+        }
+  where
+    oldAnsString = mkOldAnsString (ansN env)
+    oldAns = BS.pack oldAnsString
 
 
 define :: ByteString -> String -> Env -> Env
 define name body env =
     env { defs = Trie.insert name body (defs env) }
+  where
+    formattedBody = formatDefinition (BS.unpack name) body
+
+
+mkOldAnsString n =
+  "replOldAns" ++ show n
 
 
 display :: String -> Env -> Env
 display body env =
-    define lastVar (format body) env
+  if lastVarString == body then
+    env
+  else if lastVarString `isSubsequenceOf` body then
+    defineAns (replaceAns env body) env
+  else
+    defineAns (body) env
+
+
+replaceAns env s =
+    unwords $ map (\w -> if w == lastVarString then oldAnsString else w) $ words s
   where
-    format body =
-        lastVarString ++ " =" ++ concatMap ("\n  "++) (lines body)
+    oldAnsString =
+      mkOldAnsString $ pred (ansN env)
+
+
+formatDefinition :: String -> String -> String
+formatDefinition name body =
+    name ++ " =" ++ concatMap ("\n  "++) (lines body)
 
 
 noDisplay :: Env -> Env
